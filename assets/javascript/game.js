91d0e5a75716d855fb821df410b62497c2dba559
bob <antfats@hotmail.com>
2019-03-03 19:48:53 -0700

    Start of the new hw

---
+++ assets/javascript/game.js
@@ -0,0 +1,5 @@
+var animals = ["cow","cat","dog","bird","frog","turtle","snake","lizard"];
+var buttons = $("<button btn-primary class='buttons'>");
+//cycle through all of the animals array, create a new button
+//append the new button to the html
+//make sure the button says the name of the animal
