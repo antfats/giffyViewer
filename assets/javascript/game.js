9dd0658560d87268828a668c0630b80255c05100
bob <antfats@hotmail.com>
2019-03-04 14:29:03 -0700

    Start of giffy hw

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,47 @@
-var animals = ["cow","cat","dog","bird","frog","turtle","snake","lizard"];
+var animals = ["cow", "cat", "dog", "bird", "frog", "turtle", "snake", "lizard"];
 var buttons = $("<button btn-primary class='buttons'>");
-//cycle through all of the animals array, create a new button
-//append the new button to the html
-//make sure the button says the name of the animal
+var userInput = $("#animalSearch")
+//gets the animals data to see which kind of animal it is
+//plugs it into the queryURL to connect with the API
+//pulls 5 articles according to which button was clicked
+//building URL here
+$("button").click(function () {
+
+    var animal = $(this).attr("data-animal");
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=BvuUHWTWwn7CPLog3UNY8m4bY4HzmW9X&limit=5";
+
+
+    //ajax call
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+    
+
+        //Trying to pring out the specific name of the animal based on which button was clicked
+        $(".buttons").click(function(){
+          
+
+
+
+
+        })
+
+
+
+        //New buttons are created according to what the user inputs
+        
+        $("#submitBtn").click(function () {
+            var newBtn = $("<button class='btn btn-info'>");
+            var userSearch = userInput[0].value;
+            animals.push(userSearch);
+            newBtn.attr(userSearch);
+            newBtn.text(userSearch);
+            $(".buttonsHere").prepend(newBtn);
+
+        })
+
+    })
+});
+
+
