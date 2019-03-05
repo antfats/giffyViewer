8e1791f3e176f7cee7e91af15ae4e4ec86588cbe
bob <antfats@hotmail.com>
2019-03-05 12:08:48 -0700

    got pictures to load!

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,34 +10,59 @@ $("button").click(function () {
     var animal = $(this).attr("data-animal");
     var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=BvuUHWTWwn7CPLog3UNY8m4bY4HzmW9X&limit=5";
 
-
-    //ajax call
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
-    
+        console.log(response);
 
+        var results = response.data;
+
+
+
+
+        for (var i = 0; i < results.length; i++) {
+            var animalDiv = $("<div>");
+            var p = $("<p>").text("Rating: " + results[i].rating)
+            var animalImage = $("<img>")
+            animalImage.attr("src", results[i].images.fixed_height.url)
+            animalDiv.append(p);
+            animalDiv.append(animalImage)
+            $("#imagesHere").prepend(animalDiv);
+        }
+
+        //Create a button for every item in the array
+        if (1 < 2) {
+            for (var i = 0; i < animals.length; i++) {
+                var newBtn = $("<button class='btn btn-info'>");
+                var usersearch = $("#animalSearch").value
+                var userSearch = $("#animalSearch").value
+                console.log(usersearch);
+                console.log(userSearch);
+                
+                newBtn.attr(userSearch);
+                newBtn.text(userSearch);
+                $(".buttonsHere").prepend(newBtn);
+            }
+        }
         //Trying to pring out the specific name of the animal based on which button was clicked
-        $(".buttons").click(function(){
-          
-
-
-
+        $(".buttons").click(function () {
 
         })
 
 
-
-        //New buttons are created according to what the user inputs
-        
+        //Users input is added to the array
         $("#submitBtn").click(function () {
-            var newBtn = $("<button class='btn btn-info'>");
-            var userSearch = userInput[0].value;
+            var userSearch = $("#animalSearch").value
             animals.push(userSearch);
-            newBtn.attr(userSearch);
-            newBtn.text(userSearch);
-            $(".buttonsHere").prepend(newBtn);
+
+
+            // var newBtn = $("<button class='btn btn-info'>");
+            // var userSearch = userInput[0].value;
+            // var usersearch = $("#animalSearch").value
+            // newBtn.attr(userSearch);
+            // newBtn.text(userSearch);
+            // $(".buttonsHere").prepend(newBtn);
 
         })
 
