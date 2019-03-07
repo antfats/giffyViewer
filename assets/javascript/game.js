d5a4bd2dc242ece1f36d50f7f64b5da31a0ee3e1
bob <antfats@hotmail.com>
2019-03-07 15:10:35 -0700

    Strugglin a bit but should be okay!

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,12 @@
 var animals = ["cow", "cat", "dog", "bird", "frog", "turtle", "snake", "lizard"];
-var buttons = $("<button btn-primary class='buttons'>");
-var userInput = $("#animalSearch")
 //gets the animals data to see which kind of animal it is
 //plugs it into the queryURL to connect with the API
 //pulls 5 articles according to which button was clicked
 //building URL here
-$("button").click(function () {
+
+
+
+$(document).ready(function () {
 
     var animal = $(this).attr("data-animal");
     var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=BvuUHWTWwn7CPLog3UNY8m4bY4HzmW9X&limit=5";
@@ -15,58 +16,43 @@ $("button").click(function () {
         method: "GET"
     }).then(function (response) {
         console.log(response);
-
         var results = response.data;
 
+        $("#submitBtn").click(function () {
+            event.preventDefault();
+            var userSearch = $("#animalss").val().trim();
+            animals.push(userSearch);
+            console.log(animals);
+        });
+        //Create buttons with the value of the input
+        //all animal buttons need a class, so the .click can pick them all  up
+        //search the document.body for the class on the buttons, so the webpage can dynamically detect the new buttons
+        //when the buttons are clicked run the api and grab the pictures and put them in a container with the rating of the movie above it
+        //prepend the images to the imagesHere div
 
 
 
-        for (var i = 0; i < results.length; i++) {
-            var animalDiv = $("<div>");
-            var p = $("<p>").text("Rating: " + results[i].rating)
-            var animalImage = $("<img>")
-            animalImage.attr("src", results[i].images.fixed_height.url)
-            animalDiv.append(p);
-            animalDiv.append(animalImage)
-            $("#imagesHere").prepend(animalDiv);
-        }
 
-        //Create a button for every item in the array
-        if (1 < 2) {
+
+        function createAllBtns() {
             for (var i = 0; i < animals.length; i++) {
                 var newBtn = $("<button class='btn btn-info'>");
-                var usersearch = $("#animalSearch").value
-                var userSearch = $("#animalSearch").value
-                console.log(usersearch);
-                console.log(userSearch);
-                
-                newBtn.attr(userSearch);
-                newBtn.text(userSearch);
-                $(".buttonsHere").prepend(newBtn);
+                newBtn.attr("data-animal=" + animals[i].value)
+                $(".buttonsHere").append(newBtn);
+
             }
         }
-        //Trying to pring out the specific name of the animal based on which button was clicked
-        $(".buttons").click(function () {
 
-        })
-
-
-        //Users input is added to the array
-        $("#submitBtn").click(function () {
-            var userSearch = $("#animalSearch").value
-            animals.push(userSearch);
-
-
-            // var newBtn = $("<button class='btn btn-info'>");
-            // var userSearch = userInput[0].value;
-            // var usersearch = $("#animalSearch").value
-            // newBtn.attr(userSearch);
-            // newBtn.text(userSearch);
-            // $(".buttonsHere").prepend(newBtn);
-
-        })
-
-    })
+        function printImg() {
+            for (var i = 0; i < results.length; i++) {
+                var animalDiv = $("<div>");
+                var p = $("<p>").text("Rating: " + results[i].rating)
+                var animalImage = $("<img>")
+                animalImage.attr("src", results[i].images.fixed_height.url)
+                animalDiv.append(p);
+                animalDiv.append(animalImage)
+                $("#imagesHere").prepend(animalDiv);
+            }
+        }
+    });
 });
-
-
